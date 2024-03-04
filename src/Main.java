9737347fd4757f4e641c1969c2df8c168b8c77ec
Brianmax <george.maxi@ucsp.edu.pe>
2024-03-04 18:47:23 -0500

    main

--- src/Main.java
+++ src/Main.java
@@ -13,25 +13,3 @@ public class Main {
         }
     }
 }
-
-mport java.util.Scanner; import java.util.Random;
-public class ejercicio4 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-        int numeroAleatorio = random.nextInt(100) + 1;
-        int intentos = 0;
-        int intentoUsuario;
-        System.out.println("Bienvenido al juego de adivinar el número");
-        System.out.println("Intenta adivinar un número entre 1 y 100.");
-        do {
-            System.out.print("Introduce tu intento: ");
-            intentoUsuario = scanner.nextInt();
-            intentos++;
-            if (intentoUsuario < numeroAleatorio) {
-                System.out.println("El número es demasiado bajo. ¡Inténtalo de nuevo!");
-            } else if (intentoUsuario > numeroAleatorio) {
-                System.out.println("El número es demasiado alto. ¡Inténtalo de nuevo!");
-            }
-        } while (intentoUsuario != numeroAleatorio);
-                System.out.println("¡Fe
